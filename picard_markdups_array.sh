6411d22fce22faf678bc96d9ea2799967da16ec0
Grace Fang <115444762+fc87290118@users.noreply.github.com>
2026-04-15 14:58:21 +0800

    Add files via upload
    step 2 after mapping and sorting, mark duplicates

--- picard_markdups_array.sh
+++ picard_markdups_array.sh
@@ -4,9 +4,10 @@
 #SBATCH --mem=32G
 #SBATCH --time=12:00:00
 #SBATCH --out=markdups_index_%A_%a.out
-#SBATCH --array=1-14
+#SBATCH --array=2-662
 #SBATCH --mail-user=grace.fang@curtin.edu.au
 #SBATCH --mail-type=ALL
+#SBATCH --account=pawsey1142
 
 set -euo pipefail
 
@@ -15,25 +16,25 @@ module load singularity/4.1.0-slurm
 singularity pull --force https://depot.galaxyproject.org/singularity/picard:3.4.0--hdfd78af_0
 
 #set SAMPLE_LIST
-SAMPLE_LIST="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/sample_list_markdups"
+SAMPLE_LIST="/scratch/pawsey1142/gracefang/WPM_project/WPM_population_genetics/sample_list_markdups"
 
 #reald SAMPLE_LIST
 read SAMPLE BAM <<< "$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SAMPLE_LIST")"
 
 echo "[$(date)] Processing sample: $SAMPLE"
-echo "Input SAM: $BAM"
+echo "Input BAM: $BAM"
 
 # Output BAM in the same directory as the SAM
-OUTDIR="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/3b.sort_removeDuplicates"
+OUTDIR="/scratch/pawsey1142/gracefang/WPM_project/WPM_population_genetics/2b.MarkDuplicates"
 OUTPUT="${OUTDIR}/${SAMPLE}.markdup.bam"
-METRIX="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/3b.sort_removeDuplicates/$SAMPLE.marked_dup_metrics.txt"
+METRIX="/scratch/pawsey1142/gracefang/WPM_project/WPM_population_genetics/2b.MarkDuplicates/$SAMPLE.marked_dup_metrics.txt"
 
 # run singularity
 singularity run picard:3.4.0--hdfd78af_0 \
-picard MarkDuplicates \
-I=$BAM \
-O=$OUTPUT \
-M=$METRIX \
-TAGGING_POLICY=All
+  picard MarkDuplicates \
+    I="$BAM" \
+    O="$OUTPUT" \
+    M="$METRIX" \
+    TAGGING_POLICY=All
 
 echo "MarkDuplicates completed successfully."
