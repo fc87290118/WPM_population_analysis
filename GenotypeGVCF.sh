cb2866140d8c843fe62c2a2b12cf0fbb8d913b40
Grace Fang <115444762+fc87290118@users.noreply.github.com>
2026-05-07 13:54:00 +0800

    Add files via upload

--- GenotypeGVCF.sh
+++ GenotypeGVCF.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 #SBATCH --job-name=GenotypeGVCFs
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=16G
-#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --time=3-00:00:00
+#SBATCH --partition=long
 #SBATCH --output=GenotypeGVCFs_%j.out
 #SBATCH --error=GenotypeGVCFs_%j.err
 #SBATCH --mail-user=grace.fang@curtin.edu.au
 #SBATCH --mail-type=ALL
+#SBATCH --account=pawsey1142
+
 
 set -euo pipefail
 # ==== MODULE ====
 module load singularity/4.1.0-slurm
 #singularity pull --force https://depot.galaxyproject.org/singularity/gatk4:4.6.2.0--py310hdfd78af_1
 
-GVCF="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/7b.GVCF/combined_batch1.g.vcf.gz"
-OUTPUT_FILE="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/7b.GVCF/genotypeGVCF.vcf.gz"
-FASTA="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/060.flye.corrected.medaka.ntLink.k25.w300.rounds5.scaffolds.gap_fill.polypolish.fasta"
+GVCF="/scratch/pawsey1142/gracefang/WPM_project/WPM_population_genetics/7b.GenotypeGVCFs/genotypeGVCF.v3.vcf.gz"
+OUTPUT_FILE="/scratch/pawsey1142/gracefang/WPM_project/WPM_population_genetics/7b.GenotypeGVCFs/genotypeGVCF.v3.vcf.gz"
+FASTA="/scratch/pawsey1142/gracefang/WPM_project/WPM_genome/Bgt_USA_2_genome_v1.fasta"
 
 echo "[$(date)] Running GenotypeGVCFs on:"
 echo "  GVCF: $GVCF"
 echo "  FASTA: $FASTA"
 echo "  Output: $OUTPUT_FILE"
 
+# ==== INDEX ====
+singularity run gatk4:4.6.2.0--py310hdfd78af_1 \
+  gatk IndexFeatureFile \
+    -I "$GVCF"
+
 # ==== RUN GATK GenotypeGVCF ====
 singularity run gatk4:4.6.2.0--py310hdfd78af_1 \
-     gatk --java-options "-Xmx4g" GenotypeGVCFs \
+  gatk --java-options "-Xmx48g" GenotypeGVCFs \
     -R "$FASTA" \
     -V "$GVCF" \
     -O "$OUTPUT_FILE" \
     --max-alternate-alleles 4
 
-      echo "[$(date)] GenotypeGVCF susscessful"
+echo "[$(date)] GenotypeGVCF susscessful"
--- SelectVariantsSNP_INDELs.sh
+++ SelectVariantsSNP_INDELs.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 #SBATCH --job-name=VariantFiltration
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=16G
-#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
 #SBATCH --output=VariantFiltration_%j.out
 #SBATCH --error=VariantFiltration_%j.err
 #SBATCH --mail-user=grace.fang@curtin.edu.au
 #SBATCH --mail-type=ALL
+#SBATCH --account=pawsey1142
 
 set -euo pipefail
 # ==== MODULE ====
 module load singularity/4.1.0-slurm
 #singularity pull --force https://depot.galaxyproject.org/singularity/gatk4:4.6.2.0--py310hdfd78af_1
 
-REF="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/060.flye.corrected.medaka.ntLink.k25.w300.rounds5.scaffolds.gap_fill.polypolish.fasta"
-IN_VCF="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/8b.VariantFiltration/genotypeGVCF.filtered_all.vcf.gz"
-OUT_DIR="/scratch/y95/gracefang/WPM_project/WPM_population_genetics/8b.VariantFiltration"
+REF="/scratch/pawsey1142/gracefang/WPM_project/WPM_genome/Bgt_USA_2_genome_v1.fasta"
+IN_VCF="/scratch/pawsey1142/gracefang/WPM_project/WPM_population_genetics/8b.VariantFiltration/genotypeGVCF_filtered_all.vcf.gz"
+OUT_DIR="/scratch/pawsey1142/gracefang/WPM_project/WPM_population_genetics/8b.VariantFiltration"
 
 echo "[$(date)] Input VCF: $IN_VCF"
 
 # ==== 1) SNPs: PASS only ====
 echo "[$(date)] Selecting PASS SNPs..."
 singularity run gatk4:4.6.2.0--py310hdfd78af_1 \
-  gatk SelectVariants \
+  gatk --java-options "-Xmx16g" SelectVariants \
     -R "$REF" \
     -V "$IN_VCF" \
     --select-type-to-include SNP \
@@ -32,7 +33,7 @@ singularity run gatk4:4.6.2.0--py310hdfd78af_1 \
 # ==== 2) INDELs: PASS only ====
 echo "[$(date)] Selecting PASS INDELs..."
 singularity run gatk4:4.6.2.0--py310hdfd78af_1 \
-  gatk SelectVariants \
+  gatk --java-options "-Xmx16g" SelectVariants \
     -R "$REF" \
     -V "$IN_VCF" \
     --select-type-to-include INDEL \
@@ -47,6 +48,7 @@ echo "  INDELs -> ${OUT_DIR}/indels_pass_variantfiltration.vcf.gz"
 echo "[$(date)] Counting variants..."
 
 module load singularity/4.1.0-slurm  # ensure bcftools container uses same module
+singularity pull --force https://depot.galaxyproject.org/singularity/bcftools:1.9--ha228f0b_4
 
 SNP_COUNT=$(singularity run bcftools:1.9--ha228f0b_4 \
     bcftools view -H "${OUT_DIR}/snps_pass_variantfiltration.vcf.gz" | wc -l)
